9f9b17372806d817b2b8cf79b0e2e05bbe33fc32
Abdul Matin <matinict@gmail.com>
2017-03-22 22:04:11 +0600

    Create evenOdd.c

---
+++ evenOdd.c
@@ -0,0 +1,14 @@
+
+
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter an Integer Number: \n");
+    scanf("%d",&n);
+    
+    n%2==0?printf("Even\n"):printf("Odd\n");
+    return 0;
+
+}
