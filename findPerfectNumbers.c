3158836e31a64cd3a31f837ee91279a01e04ee46
Abdul Matin <matinict@gmail.com>
2017-03-26 23:03:48 +0600

    Update findPerfectNumbers.c

--- findPerfectNumbers.c
+++ findPerfectNumbers.c
@@ -4,25 +4,18 @@
 #include <stdio.h>
 main(void){
     int n,i,sum, min,max;
-    
-    printf("Enter the minimum range: ");
-    scanf("%d",&min);
-    
-    printf("Enter the maximum range: ");
-    scanf("%d",&max);
+    printf("Enter the minimum range: ");scanf("%d",&min);
+    printf("Enter the maximum range: ");scanf("%d",&max);
     
     printf("Perfect numbers in given range is: ");
     for(n=min;n<=max;n++){
         i=1;sum = 0;
-        
         while(i<n){
             if(n%i==0){sum=sum+i;}
             i++;
         }
-        
         if(sum==n){printf("%d ",n);}
     }
-
 }
 //Enter the minimum range: 1
 //Enter the maximum range: 1111
