87a5c3546a189d9533c15198759eb7d00cb8230f
Abdul Matin <matinict@gmail.com>
2017-03-27 21:35:08 +0600

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -6,8 +6,13 @@ main(void){
         printf("Enter the size of array: ");scanf("%d",&n);
         printf("Enter the array elements: ");
         for(i=0;i<n;++i){ scanf("%d",&a[i]);}
+        
+         // Outer loop - need n iteration to sort n elements
         for(i=1;i<n;++i){
+               //Inner loop to perform comparision and swapping between adjacent numbers
+            //After each iteration one index from last is sorted
             for(j=0;j<(n-i);++j)
+                    //If current number is greater than swap those two
                 if(a[j]>a[j+1]) {temp=a[j];a[j]=a[j+1]; a[j+1]=temp;}
         }
         printf("\nArray after sorting: ");
